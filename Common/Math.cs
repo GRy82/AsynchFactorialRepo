f54bb980c6c28d05664813a0e87327f731b87c6e
GRy82 <gwryan@buffalo.edu>
2021-03-18 09:43:36 -0400

    WPF application created. C# Library project created within the same solutions. Has a Math class with Factorial recursive function within.

---
+++ Common/Math.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    class Math
+    {
+        public static ulong Factorial(ulong number)
+        {
+            System.Threading.Thread.Sleep(25);
+
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            return number * Factorial(number - 1);
+        }
+    }
+}
